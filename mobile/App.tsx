7a4ffb8536690fec9e2ddfa5b6fb75bbb684807c
Eduardo Assunção <cunhaeduardo1231@gmail.com>
2022-09-18 13:57:55 -0300

    feat(mobile): implements push notifications

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,7 @@
+import * as Notifications from 'expo-notifications';
+import { Subscription } from 'expo-modules-core';
 import { StatusBar } from 'react-native';
+import { useEffect, useRef } from 'react';
 import {
   useFonts,
   Inter_400Regular,
@@ -11,6 +14,9 @@ import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
 
+import './src/services/notificationConfig';
+import { getPushNotificationsToken } from './src/services/getPushNotification';
+
 export default function App() {
   const [isFontLoaded] = useFonts({
     Inter_400Regular,
@@ -18,6 +24,39 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black
   });
+
+  const getNotificationsListener = useRef<Subscription>();
+  const responseNotificationsListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationsToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationsListener.current =
+      Notifications.addNotificationReceivedListener(notification => {
+        console.log(notification);
+      });
+
+    responseNotificationsListener.current =
+      Notifications.addNotificationResponseReceivedListener(response => {
+        console.log(response);
+      });
+
+    return () => {
+      if(
+        getNotificationsListener.current &&
+        responseNotificationsListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationsListener.current
+        );
+        Notifications.removeNotificationSubscription(
+          responseNotificationsListener.current
+        );
+      }
+    }
+  }, []);
 
   return (
     <Background>
